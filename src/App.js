c1527d71694fcba841d886fe1eeff9784452be9c
Nawaz9191 <nawaz@itradesoft.com>
2021-08-09 17:03:14 +0530

    react redux sample counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router ,Route, Switch,Link} from 'react-router-dom';
+import Link1 from './Link1';
+import Link2 from './Link2';
+import Link3 from './Link3';
+import Home from './Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <h1>HEllo world</h1>
+ <Router>
+<nav>
+<Link to='/home'>Home</Link>
+<Link to='/link1'>Link1</Link>
+<Link to='/link2'>Link2</Link>
+<Link to='/link3'>Link3</Link>
+</nav>
+<div className="center">
+  <Switch>
+<Route path="/home" component={Home} />
+<Route exact path="/link1" component={Link1} />
+<Route exact path="/link2" component={Link2} />
+<Route exact path="/link3" component={Link3} />
+  </Switch>
+</div>
+ </Router>
+
     </div>
   );
 }
---
+++ src/Link3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Link3 = () => {
+    return (
+        <div>
+            Link3
+        </div>
+    )
+}
+
+export default Link3
+
